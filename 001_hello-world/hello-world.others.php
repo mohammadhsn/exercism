a95117a826859df779dd1f74856efc79f64bd3d0
Mohammad Hoseini <mohammd@Mohammads-MacBook-Pro-2.local>
2020-01-26 19:24:06 +0330

    Hello world

---
+++ 001_hello-world/hello-world.others.php
@@ -0,0 +1,14 @@
+<?php
+
+//
+// This is only a SKELETON file for the "Hello World" exercise.
+// It's been provided as a convenience to get you started writing code faster.
+//
+
+function helloWorld($name = null)
+{
+    if ($name != null) {
+        return 'Hello, ' . $name . '!';
+    }
+    return "Hello, World!";
+}
